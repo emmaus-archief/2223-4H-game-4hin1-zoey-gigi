cdffa96a71f9bdb448cb67b8b70319d62417b5be
Gigi Mok <115073785+Kiki05M@users.noreply.github.com>
2023-03-22 10:42:57 +0000

    background-color

--- script.js
+++ script.js
@@ -94,9 +94,7 @@ function setup() {
   // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
   createCanvas(1280, 720);
 
-  // Kleur de achtergrond blauw, zodat je het kunt zien
-  background('blue');
-}
+
 
 /**
  * draw
