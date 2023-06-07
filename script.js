831e34d730648a669dec7aa4caf3fd74220260e8
Kiki05M <115073785+Kiki05M@users.noreply.github.com>
2023-06-07 17:53:39 +0000

    platform tot 7

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ var spelerX = 1075; // x-positie van speler
 var spelerY = 700; // y-positie van speler
 var speler2X = 200; // x-positie van speler2
 var speler2Y = 700; // y-positie van speler2
-var health = 50;
+var health = 10;
 
 var valkuilX = 660;
 var valkuilY = 665;
@@ -56,6 +56,26 @@ var platform3X = 840;
 var platform3Y = 660;
 var platform3Hoogte = 15;
 var platform3Breedte = 90;
+
+var platform4X = 370;
+var platform4Y = 530;
+var platform4Hoogte = 15;
+var platform4Breedte = 150;
+
+var platform5X = 810;
+var platform5Y = 530;
+var platform5Hoogte = 15;
+var platform5Breedte = 150;
+
+var platform6X = 185;
+var platform6Y = 600;
+var platform6Hoogte = 15;
+var platform6Breedte = 150;
+
+var platform7X = 1215;
+var platform7Y = 600;
+var platform7Hoogte = 15;
+var platform7Breedte = 150;
 
 var spelerSpringt = false;
 var springSnelheid = 5;
@@ -115,6 +135,18 @@ var beweegAlles = function() {
     spelerSpringt = false;
   }
 
+      // speler landt op platform 4
+  if (spelerY + 1 >= platform4Y && spelerY + 10 <= platform4Y + platform4Hoogte && spelerX >= platform4X && spelerX <= platform4X + platform4Breedte) {
+    spelerY = platform4Y - 5;
+    spelerSpringt = false;
+  }
+
+      // speler landt op platform 5
+  if (spelerY + 1 >= platform5Y && spelerY + 10 <= platform5Y + platform5Hoogte && spelerX >= platform5X && spelerX <= platform5X + platform5Breedte) {
+    spelerY = platform5Y - 5;
+    spelerSpringt = false;
+  }
+
   if (spelerSpringt === false &&
     keyIsDown(UP_ARROW)) {
     spelerSpringt = true;
@@ -136,19 +168,30 @@ var beweegAlles = function() {
   if (keyIsDown(65)) {
     speler2X = speler2X - 3;
   }
-
+//speler2 landt op platform1
   if (speler2Y + 10 >= platformY && speler2Y + 10 <= platformY + platformHoogte && speler2X >= platformX && speler2X <= platformX + platformBreedte) {
     speler2Y = platformY - 10;
     speler2Springt = false;
   }
-
+//speler2 landt op platform 2
   if (speler2Y + 1 >= platform2Y && speler2Y + 1 <= platform2Y + platform2Hoogte && speler2X >= platform2X && speler2X <= platform2X + platform2Breedte) {
     speler2Y = platform2Y - 5;
     speler2Springt = false;
   }
-
+//speler2 landt op platform 3
   if (speler2Y + 1 >= platform3Y && speler2Y + 1 <= platform3Y + platform3Hoogte && speler2X >= platform3X && speler2X <= platform3X + platform3Breedte) {
     speler2Y = platform3Y - 5;
+    speler2Springt = false;
+  }
+//speler2 landt op platform 4
+    if (speler2Y + 1 >= platform4Y && speler2Y + 1 <= platform4Y + platform4Hoogte && speler2X >= platform4X && speler2X <= platform4X + platform4Breedte) {
+    speler2Y = platform4Y - 5;
+    speler2Springt = false;
+  }
+
+  //speler2 landt op platform 5
+    if (speler2Y + 1 >= platform5Y && speler2Y + 1 <= platform5Y + platform5Hoogte && speler2X >= platform5X && speler2X <= platform5X + platform5Breedte) {
+    speler2Y = platform5Y - 5;
     speler2Springt = false;
   }
   
@@ -200,33 +243,62 @@ var tekenAlles = function() {
   // achtergrond
   fill("lightpink");
   rect(0, 0, 1280, 720);
+  
   // speler 1
   image(imgP1, speler2X - 35, speler2Y - 80, 70, 90);
   fill("black");
   ellipse(speler2X, speler2Y, 10, 10);
+  
   //speler 2
   image(imgP2, spelerX - 30, spelerY - 75, 60, 80);
   fill("black");
   ellipse(spelerX, spelerY, 10, 10);
+  
   //valkuil
   fill("red");
   rect(valkuilX - 14, valkuilY - 50, 28, 100);
   fill("yellow");
   ellipse(valkuilX, valkuilY, 10, 10);
+  
   // punten en health
+  
   //platform
   image(imgcloud, platformX - 96, platformY - 182, 400, 400);
   fill("black");
   rect(platformX, platformY, platformBreedte, 15);
+  
   //platform2
   image(imglongcloud, platform2X - 130, platform2Y - 182, 400, 400);
   fill("black");
   rect(platform2X, platform2Y, platform2Breedte, 15);
+  
   //platform3
   image(imgcloud, platform3X - 96, platform3Y - 182, 400, 400);
   fill("black");
   rect(platform3X, platform3Y, platform3Breedte, 15);
-};
+  
+  //platform4
+  image(imglongcloud, platform4X - 130, platform4Y - 182, 400, 400);
+  fill("black");
+  rect(platform4X, platform4Y, platform4Breedte, 15);
+  
+  //platform5
+  image(imglongcloud, platform5X - 130, platform5Y - 182, 400, 400);
+  fill("black");
+  rect(platform5X, platform5Y, platform5Breedte, 15);
+
+
+  //platform6
+  image(imglongcloud, platform6X - 130, platform6Y - 182, 400, 400);
+  fill("black");
+  rect(platform6X, platform6Y, platform6Breedte, 15);
+
+
+  //platform7
+  image(imglongcloud, platform7X - 130, platform7Y - 182, 400, 400);
+  fill("black");
+  rect(platform7X, platform7Y, platform7Breedte, 15);
+}
 
 /**
  * return true als het gameover is
